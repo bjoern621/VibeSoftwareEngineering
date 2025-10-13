bce68f9552e6ed6aa485bea599506497a9b33373
mario <mario.hansen@haw-hamburg.de>
2025-10-13 20:50:13 +0200

    Revert "WeekView hinzugefügt (Mo-Fr)"
    This reverts commit cb67989068958893fe1139c0f6df7236b7f9a911.

--- MyMensa/Frontend/src/App.js
+++ MyMensa/Frontend/src/App.js
@@ -1,27 +1,9 @@
-import React from 'react';
-import './App.css';
-import WeekView from './components/WeekView';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      {/* Header für die gesamte Anwendung */}
-      <header className="app-header">
-        <h1>🍽️ MyMensa</h1>
-        <p className="app-subtitle">Dein digitaler Mensaplan</p>
-      </header>
-
-      {/* Hauptinhalt */}
-      <main className="app-main">
-        <WeekView />
-      </main>
-
-      {/* Footer */}
-      <footer className="app-footer">
-        <p>© 2025 MyMensa</p>
-      </footer>
-    </div>
-  );
+      <div>
+        <Dashboard />      </div>  );
 }
 
 export default App;
